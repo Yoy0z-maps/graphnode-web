a9bccd5f0665ec755b84adbea116700db503b46b
John Han <johnhan@Johns-MacBook-Pro.local>
2026-03-12 14:19:58 +0900

    REFECTOR: 중복된 동기화 로직 제거

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,28 +1,8 @@
 import HomeTitle from "@/components/home/HomeTitle";
 import ChatSendBox from "@/components/home/ChatSendBox";
 import RecentNotes from "@/components/home/RecentNotes";
-import { useEffect } from "react";
-import { pullNotesOnce } from "@/managers/syncNoteWorker";
-import { api } from "@/apiClient";
-import { useSyncStore } from "@/store/useSyncStore";
 
 export default function Home({ username }: { username: string }) {
-  const { isSyncronized, setIsSyncronized } = useSyncStore();
-
-  useEffect(() => {
-    (async () => {
-      await pullNotesOnce();
-      // 서버 데이터를 가져와서 로컬 데이터와 비교하여 동기화
-      if (!isSyncronized) {
-        const res = await api.sync.pull();
-        // TODO: 채팅 노트 비교 후 동기화 필요하면 동기화
-        if (res.isSuccess) {
-          setIsSyncronized(true);
-        }
-      }
-    })();
-  }, []);
-
   return (
     <div className="flex flex-col items-center bg-bg-primary min-h-full">
       <HomeTitle username={username} />
